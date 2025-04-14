15a2be1eba0950b80f902408b1da95700b29c4cb
cacayan2 <ecacayan@luc.edu>
2025-04-14 16:37:41 -0500

    Outputted SVA data to RDS

--- svassumption.R
+++ svassumption.R
@@ -15,45 +15,38 @@ sva_QTL <- function(TWAS_folder, member) {
   qtlcoloc <- readRDS(paste0(member, paste0(str_replace(paste0("/", member), paste0(TWAS_folder, "/"), ""), "_qtlcoloc")))
 
   sv.res <- coloc.abf(dataset1 = gwascoloc, dataset2 = qtlcoloc)
-  sv.sens <- sensitivity(sv.res, "H4 > 0.5")
-
-  return(c(sv.res, sv.sens))
 }
 
 # Define command-line options.
 option_list <- list(
   make_option("--process", type = "character", default = "pqtl", help = "If using eqtl data, add process flag, otherwise pqtl is the default."),
-  make_option("--superpop", type = "character", help = "The subpopulation to create LD matrix (generated from 1000 genomes project). See README.md for more options.")
+  make_option("--superpop", type = "character", help = "The subpopulation to create LD matrix (generated from 1000 genomes project). See README.md for more options."),
+  make_option("--output", type = "character", help = "Output directory.")
 )
 
 # Obtain command line arguments.
 opt <- parse_args(OptionParser(option_list = option_list))
-if(length(opt) != 3) {
+if(length(opt) != 4) {
   print("Please pass an appropriate number of arguments to the command line.", quote = FALSE)
   stop()
 }
 
 # Set folder for TWAS data. 
-TWAS_folder = ""
+TWAS_folder = paste0(getwd(), paste0("/", opt$output))
 
-if(opt$process == "eQTL") {
-  TWAS_folder = paste0(getwd(), "/processed/eQTL_results")
-} else {
-  TWAS_folder = paste0(getwd(), "/processed/pQTL_results")
-}
 
 message("Running coloc assuming single variant assumption...")
 # Running coloc using single variant assumption
 data_members <- list.dirs(TWAS_folder)
 data_members <- data_members[-1]
-
-sv.analysis <- new.env()
 for(member in data_members) {
+  member_name = str_replace(member, paste0(TWAS_folder, "/"), "")
   if(length(list.files(member)) == 0) {
-    message(paste0("SVA: The following result not processed - likely not an entry in QTL lists: ", str_replace(member, paste0(TWAS_folder, "/"), "")))
+    message(paste0("SVA: The following result not processed - likely not an entry in QTL lists: ", member_name))
     next
   }
-  sv.analysis[[str_replace(member, paste0(TWAS_folder, "/"), "")]] <- sva_QTL(TWAS_folder, member)
+  current_sva <- sva_QTL(TWAS_folder, member)
+  saveRDS(current_sva, file = file.path(member, paste0(member_name, "_sva"))) 
 }
 
-print("Setting up linkage disequilibrium data...")
+message("Setting up linkage disequilibrium data...")
